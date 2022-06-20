5ca5bc6373cd997420129fbdc4366477f544242b
Roman Matveev <r.matveev@socrat.marketing>
2022-06-20 13:54:47 +1000

    20.06.2022 -- Выстраивание общей структуры сервиса. Часть 4. Добавлено описание тела запроса уведобляющего о статусе платежа.

--- src/app/Services/Qiwi/PaymentService.php
+++ src/app/Services/Qiwi/PaymentService.php
@@ -76,10 +76,43 @@ class PaymentService
     }
 
     /**
+     * Тело уведомляющего запроса, приходящего от сервера qiwi
+    {
+    "bill": {
+    "siteId": "9hh4jb-00",
+    "billId": "cc961e8d-d4d6-4f02-b737-2297e51fb48e",
+    "amount": {
+    "value": "1.00",
+    "currency": "RUB"
+    },
+    "status": {
+    "value": "PAID",
+    "changedDateTime": "2021-01-18T15:25:18+03"
+    },
+    "customer": {
+    "phone": "78710009999",
+    "email": "test@example.com",
+    "account": "454678"
+    },
+    "customFields": {
+    "paySourcesFilter": "qw",
+    "themeCode": "Yvan-YKaSh",
+    "yourParam1": "64728940",
+    "yourParam2": "order 678"
+    },
+    "comment": "Text comment",
+    "creationDateTime": "2021-01-18T15:24:53+03",
+    "expirationDateTime": "2025-12-10T09:02:00+03"
+    },
+    "version": "1"
+    }
+     */
+    /**
      * @param array $body
      * @return JsonResponse
      */
     final public function billStatusNotify(array $body): JsonResponse {
+        //TODO - дописать логику проверки и перезаписи статуса оплаты
         return response()->json([], 200);
     }
 }
