eb5d02a8ef8b0be865180df1a9559b3591c230f5
Roman Matveev <r.matveev@socrat.marketing>
2022-06-21 10:25:29 +1000

    21.06.2022 -- Добавлена миграция "invoice" и модель для нее.

---
+++ src/app/Models/Invoice.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Invoice
+ * @package App\Models
+ */
+class Invoice extends Model
+{
+    public const ID = 'id';
+    public const USER_ID = 'user_id';
+    public const STATUS = 'status';
+    public const CURRENCY = 'currency';
+    public const PRICE = 'price';
+    public const COMMENT = 'comment';
+
+    public const TABLE_NAME = 'invoice';
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        self::ID, self::USER_ID, self::STATUS, self::CURRENCY, self::PRICE, self::COMMENT,
+    ];
+
+    /**
+     * @var string
+     */
+    protected $table = self::TABLE_NAME;
+}
---
+++ src/database/migrations/2022_06_20_235143_create_invoice_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Invoice;
+
+/**
+ * Class CreateInvoiceTable
+ */
+class CreateInvoiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable(Invoice::TABLE_NAME)) {
+            Schema::create(Invoice::TABLE_NAME, function (Blueprint $table) {
+                $table->uuid(Invoice::ID)->primary();
+                $table->string(Invoice::USER_ID, 200);
+                $table->string(Invoice::STATUS, 10);
+                $table->string(Invoice::CURRENCY, 10);
+                $table->double(Invoice::PRICE)->default(1);
+                $table->string(Invoice::COMMENT, 200);
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable(Invoice::TABLE_NAME)) {
+            Schema::dropIfExists(Invoice::TABLE_NAME);
+        }
+    }
+}
