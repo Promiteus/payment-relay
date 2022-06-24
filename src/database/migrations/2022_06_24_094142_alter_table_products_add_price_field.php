c9ee3314dfe5f540fe7f7d27bf5ea87ee850d7df
Roman Matveev <r.matveev@socrat.marketing>
2022-06-24 19:46:58 +1000

    24.06.2022 -- Создание обработчика покупок. Часть 5. Заполнение класса PaymentHandler.php - логика для метода createInvoice(), часть 3. Добавлено поле "price" в таблицу "products".

---
+++ src/database/migrations/2022_06_24_094142_alter_table_products_add_price_field.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Product;
+
+class AlterTableProductsAddPriceField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable(Product::TABLE_NAME)) {
+            Schema::table(Product::TABLE_NAME, function (Blueprint $table) {
+                $table->double(Product::PRICE)->default(0);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable(Product::TABLE_NAME)) {
+            Schema::table(Product::TABLE_NAME, function (Blueprint $table) {
+                $table->dropColumn(Product::PRICE);
+            });
+        }
+    }
+}
