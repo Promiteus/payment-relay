fb63fc0a1448ca807ca37d6b02d61d8fb349e374
Roman Matveev <r.matveev@socrat.marketing>
2022-06-29 22:32:21 +1000

    29.06.2022 -- Корректировка полей миграции users - подведение их под ORM модель сервиса аутентификации.

--- src/database/migrations/2022_06_19_054355_create_user_table.php
+++ src/database/migrations/2022_06_19_054355_create_user_table.php
@@ -17,13 +17,13 @@ class CreateUserTable extends Migration
         if (!Schema::hasTable(User::TABLE_NAME)) {
             Schema::create('users', function (Blueprint $table) {
                 $table->string(User::ID, 255)->primary();
-                $table->integer(User::CODE)->default(0);
+                $table->integer(User::CODE)->nullable();
                 $table->boolean(User::DISABLES)->default(false);
-                $table->string(User::PASSWORD, 255)->default('');
+                $table->string(User::PASSWORD, 255)->nullable();
                 $table->string(User::EMAIL, 50)->nullable();
-                $table->timestamp(User::REG_DATE);
+                $table->timestamp(User::REG_DATE)->nullable();
                 $table->string(User::REMEMBER_TOKEN, 255)->nullable();
-                $table->string(User::USER_NAME, 100);
+                $table->string(User::USER_NAME, 100)->nullable();
                 $table->boolean(User::VERIFIED_EMAIL)->default(false);
             });
         }
