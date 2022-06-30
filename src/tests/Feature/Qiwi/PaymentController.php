ed20688885fff6e8c904bc322a57032ca3a54945
Roman Matveev <r.matveev@socrat.marketing>
2022-07-01 09:44:01 +1000

    01.07.2022 -- Тесты PaymentController.php. Часть 1.

---
+++ src/tests/Feature/Qiwi/PaymentController.php
@@ -0,0 +1,26 @@
+<?php
+
+use Tests\TestCase;
+
+class PaymentController extends TestCase
+{
+    /*Тест отклонения счета*/
+    public function testCancelBill() {
+
+    }
+
+    /*Тест создания счета*/
+    public function testCreateBill() {
+
+    }
+
+    /*Тест получения статуса счета*/
+    public function testGetBillInfo() {
+
+    }
+
+    /*Тест на изменение статуса счета счета*/
+    public function testNotify() {
+
+    }
+}
