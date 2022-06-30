7c37f0d10b578b2258d09aad17a043ed6a28277a
Roman Matveev <r.matveev@socrat.marketing>
2022-06-30 23:22:57 +1000

    30.06.2022 -- Тесты на создание счета в базе. Часть 3. Поиск действующего счета.

--- src/tests/Feature/ProductInvoiceServiceTest.php
+++ src/tests/Feature/ProductInvoiceServiceTest.php
@@ -13,6 +13,10 @@ use Tests\TestCase;
 
 class ProductInvoiceServiceTest extends TestCase
 {
+    /**
+     * @var string
+     */
+    private string $billId;
 
     /**
      * @var ProductInvoiceService
@@ -32,6 +36,7 @@ class ProductInvoiceServiceTest extends TestCase
         parent::__construct($name, $data, $dataName);
 
         $this->productInvoiceService = app()->make(ProductInvoiceService::class);
+        $this->billId = Uuid::uuid4()->toString();
     }
 
 
@@ -59,67 +64,16 @@ class ProductInvoiceServiceTest extends TestCase
     }
 
 
-    public function testCreateInvoice(): void {
-        $this->seed();
-
-        $this->clearTodayRecords();
-
-        $billId = Uuid::uuid4()->toString();
-
-        $this->console("\nСозданеи нового счета...");
-
-        $products = Product::all()->take(3);
-
-        $productsBody = $products->map(function ($item) {
-            return [
-                Product::CODE => $item[Product::CODE],
-                Common::COUNT => 1,
-                Product::NAME => $item[Product::NAME],
-                Product::PRICE => $item[Product::PRICE]
-            ];
-        })->toArray();
 
 
-        $totalPrice = $products->map(function($product) {
-            return $product[Product::PRICE];
-        })->sum();
+    private function testFindInvoice(string $billId): void {
+        $this->console("\nПоиск действующего счета ...");
 
-
-        $invoice = [
-            Common::BILL_ID => $billId,
-            Common::AMOUNT => [
-                Common::CURRENCY => 'RUB',
-                Common::VALUE => $totalPrice,
-            ],
-            Invoice::STATUS => [
-                Common::VALUE => Common::WAITING_STATUS,
-            ],
-            Common::COMMENT => 'any comment',
-        ];
-
-        $order = [
-            Common::USER_ID => UsersTableSeeder::TEST_USER_ID,
-            Common::PRODUCTS => $productsBody,
-            Common::BILL_ID => $billId,
-            Common::TOTAL_PRICE => 100.0,
-        ];
-
-        /*Создать запись в таблицах invoice и product_invoice*/
-        $result =  $this->productInvoiceService->createInvoice($invoice, $order);
+        $result =  $this->productInvoiceService->findInvoice(UsersTableSeeder::TEST_USER_ID, $billId);
 
         $this->assertTrue(count($result) > 0);
 
-        /*Счет должен быть один*/
-        $invoice = Invoice::query()->where(Invoice::ID, '=', $billId)->get();
-        $this->console('Количество счетов: '.$invoice->count());
-        $this->assertTrue($invoice->count() === 1);
-
-        /*Записей по счету billId в таблице product_invoice должно быть три*/
-        $productInvoice = ProductInvoice::query()->where(ProductInvoice::INVOICE_ID, '=', $billId)->get();
-        $this->console('Количество записей в '.ProductInvoice::TABLE_NAME.': '.$productInvoice->count());
-        $this->assertTrue($productInvoice->count() === 3);
-
-        if (($productInvoice->count() === 3) && ($invoice->count() === 1)) {
+        if (count($result)) {
             $this->okMsg();
         }
     }
@@ -221,6 +175,71 @@ class ProductInvoiceServiceTest extends TestCase
             $this->okMsg($e->getMessage());
             $this->assertTrue($e->getMessage() !== '');
         }
+    }
+
+    public function testCreateInvoice(): void {
+        $this->seed();
+
+        $this->clearTodayRecords();
+
+        $this->console("\nСозданеи нового счета...");
+
+        $products = Product::all()->take(3);
+
+        $productsBody = $products->map(function ($item) {
+            return [
+                Product::CODE => $item[Product::CODE],
+                Common::COUNT => 1,
+                Product::NAME => $item[Product::NAME],
+                Product::PRICE => $item[Product::PRICE]
+            ];
+        })->toArray();
+
+
+        $totalPrice = $products->map(function($product) {
+            return $product[Product::PRICE];
+        })->sum();
+
+
+        $invoice = [
+            Common::BILL_ID => $this->billId,
+            Common::AMOUNT => [
+                Common::CURRENCY => 'RUB',
+                Common::VALUE => $totalPrice,
+            ],
+            Invoice::STATUS => [
+                Common::VALUE => Common::WAITING_STATUS,
+            ],
+            Common::COMMENT => 'any comment',
+        ];
+
+        $order = [
+            Common::USER_ID => UsersTableSeeder::TEST_USER_ID,
+            Common::PRODUCTS => $productsBody,
+            Common::BILL_ID => $this->billId,
+            Common::TOTAL_PRICE => 100.0,
+        ];
+
+        /*Создать запись в таблицах invoice и product_invoice*/
+        $result = $this->productInvoiceService->createInvoice($invoice, $order);
+
+        $this->assertTrue(count($result) > 0);
+
+        /*Счет должен быть один*/
+        $invoice = Invoice::query()->where(Invoice::ID, '=', $this->billId)->get();
+        $this->console('Количество счетов: '.$invoice->count());
+        $this->assertTrue($invoice->count() === 1);
+
+        /*Записей по счету billId в таблице product_invoice должно быть три*/
+        $productInvoice = ProductInvoice::query()->where(ProductInvoice::INVOICE_ID, '=', $this->billId)->get();
+        $this->console('Количество записей в '.ProductInvoice::TABLE_NAME.': '.$productInvoice->count());
+        $this->assertTrue($productInvoice->count() === 3);
+
+        if (($productInvoice->count() === 3) && ($invoice->count() === 1)) {
+            $this->okMsg();
+        }
+
+        $this->testFindInvoice($this->billId);
     }
 
 }
